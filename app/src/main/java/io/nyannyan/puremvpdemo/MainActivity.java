1f74335f1e0dd110eb9e42cb05c53339734ab9c7
JohanNyan <johanadam95@gmail.com>
2018-06-01 15:08:51 +0800

    Add butterknife

--- app/src/main/java/io/nyannyan/puremvpdemo/MainActivity.java
+++ app/src/main/java/io/nyannyan/puremvpdemo/MainActivity.java
@@ -3,56 +3,55 @@ package io.nyannyan.puremvpdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.nyannyan.puremvpdemo.DetailsContract.Presenter;
+import io.nyannyan.puremvpdemo.DetailsContract.View;
 
-public class MainActivity extends AppCompatActivity implements DetailsContract.View {
+public class MainActivity extends AppCompatActivity implements View {
 
-  private EditText firstName, lastName;
-  private TextView tvDetail;
-  private Button btnSubmit;
+  @BindView(R.id.tvMessage)
+  TextView tvMessage;
+  @BindView(R.id.etFirstName)
+  EditText etFirstName;
+  @BindView(R.id.etLastName)
+  EditText etLastName;
+  @BindView(R.id.btnSubmit)
+  Button btnSubmit;
 
-  private DetailsContract.Presenter presenter;
+//  private EditText firstName, lastName;
+//  private TextView tvDetail;
+//  private Button btnSubmit;
+
+  private Presenter presenter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
-    firstName = findViewById(R.id.etFirstName);
-    lastName = findViewById(R.id.etLastName);
-    tvDetail = findViewById(R.id.tvMessage);
-    btnSubmit = findViewById(R.id.btnSubmit);
-
-    presenter= new DetailsPresenter(this);
-
-    btnSubmit.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-
-        if (TextUtils.isEmpty(firstName.getText().toString())){
-
-          Toast.makeText(MainActivity.this, "Please enter first name", Toast.LENGTH_SHORT).show();
-
-        } else {
-
-          presenter.submitName(firstName.getText().toString(), lastName.getText().toString());
-          presenter.loadMessage();
-
-        }
-
-      }
-    });
-
+    presenter = new DetailsPresenter(this);
 
   }
 
   @Override
   public void showMessage(String message) {
-    tvDetail.setText(message);
+    tvMessage.setText(message);
+  }
+
+  @OnClick(R.id.btnSubmit)
+  public void onClick() {
+    if (TextUtils.isEmpty(etFirstName.getText().toString())){
+          Toast.makeText(MainActivity.this, "Please enter first name", Toast.LENGTH_SHORT).show();
+        } else {
+          presenter.submitName(etFirstName.getText().toString(), etLastName.getText().toString());
+          presenter.loadMessage();
+        }
   }
 }
